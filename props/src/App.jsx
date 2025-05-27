d555399c5aec598422bf72895ca58223f514ffea
asra <asra.khan2307@gmail.com>
2025-05-27 21:26:50 +0530

    props feature

---
+++ props/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the App</h1>
+      <Greeting name="John" />
+      <Greeting name="Brad" />
+    </>
+  );
+}
+
+export default App;
